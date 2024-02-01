858015c2588dda5349cbded8e076e8a35921e28e
Fatma Janna <fatma.janna@quinyx.com>
2024-02-01 07:17:14 +0700

    flask app init

---
+++ app/extensions.py
@@ -0,0 +1,24 @@
+from flask_bcrypt import Bcrypt
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api, Namespace
+
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+authorizations = {
+    'bearer': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'Token'
+    }
+}
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+api = Api()
+migrate = Migrate()
+debug_toolbar = DebugToolbarExtension()
+ns = Namespace("api", authorizations=authorizations)
