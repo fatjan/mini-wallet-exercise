73ade29446e9b156d4f6c12ace18496d22ef3889
Fatma Janna <fatma.janna@quinyx.com>
2024-02-01 09:20:26 +0700

    view_wallet_balance controller and service

--- app/main/controller/wallet_controller.py
+++ app/main/controller/wallet_controller.py
@@ -1,6 +1,6 @@
 from flask_restx import Resource
 from flask import request
-from ..service.wallet_service import init_wallet, enable_wallet
+from ..service.wallet_service import init_wallet, enable_wallet, view_wallet_balance
 from ...extensions import ns
 from ..util.dto import WalletDto
 from ..util.verify_token import token_required
@@ -31,3 +31,11 @@ class Wallet(Resource):
         """Enable a wallet"""
         customer_id = decoded_token["customer_id"]
         return enable_wallet(customer_id)
+    
+    @ns.doc(security="bearer")
+    @ns.doc(responses={400: "Validation Error"})
+    @token_required
+    def get(self, decoded_token):
+        """View wallet balance"""
+        customer_id = decoded_token["customer_id"]
+        return view_wallet_balance(customer_id)
--- app/main/service/wallet_service.py
+++ app/main/service/wallet_service.py
@@ -25,3 +25,16 @@ def enable_wallet(customer_id):
     except Exception as e:
         log.error(f"Error in enable_wallet: {str(e)}")
         return {"status": "error", "message": "Internal Server Error"}, 500
+
+
+def view_wallet_balance(customer_id):
+    try:
+        wallet = wallet_model.view_wallet_balance(customer_id)
+        if not wallet:
+            return {"status": "error", "message": "Wallet not found"}, 404
+
+        response_object = {"data": {"wallet": wallet}, "status": "success"}
+        return response_object, 200
+    except Exception as e:
+        log.error(f"Error in view_wallet_balance: {str(e)}")
+        return {"status": "error", "message": "Internal Server Error"}, 500
