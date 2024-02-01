86c9f83dfc1c817efc1bae878295c55bd41ecb23
Fatma Janna <fatma.janna@quinyx.com>
2024-02-01 08:37:26 +0700

    add service layer

---
+++ app/main/service/wallet_service.py
@@ -0,0 +1,36 @@
+import logging as log
+from app.main.model.wallet import Wallet
+
+wallet_model = Wallet()
+
+
+def init_wallet(customer_id):
+    try:
+        token = wallet_model.init_wallet(customer_id)
+        response_object = {
+            "data": {
+                "token": token
+            },
+            "status": "success"
+        }
+        return response_object, 201
+    except Exception as e:
+        log.error(f"Error in init_report: {str(e)}")
+        return {"status": "error", "message": "Internal Server Error"}, 500
+
+def enable_wallet(customer_id):
+    try:
+        wallet = wallet_model.enable_wallet(customer_id)
+        if not wallet:
+            return {"status": "error", "message": "Wallet not found"}, 404
+
+        response_object = {
+            "data": {
+                "wallet": wallet
+            },
+            "status": "success"
+        }
+        return response_object, 200
+    except Exception as e:
+        log.error(f"Error in enable_wallet: {str(e)}")
+        return {"status": "error", "message": "Internal Server Error"}, 500
