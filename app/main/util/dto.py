6b834894c807fe1df0753e8058846abb109388e0
Fatma Janna <fatma.janna@quinyx.com>
2024-02-01 08:37:13 +0700

    update dto to use form_parser

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -1,4 +1,4 @@
-from flask_restx import fields
+from flask_restx import fields, reqparse
 from app.extensions import api
 
 """
@@ -7,24 +7,12 @@ DTO (Data Transfer Object) is a simple object which is used to pass data between
 
 
 class WalletDto:
-    init = api.model(
-        "init",
-        {
-            "customer_xid": fields.String(required=True, description="Customer xid of the wallet owner"),
-        },
-    )
+    init_form_parser = reqparse.RequestParser()
+    init_form_parser.add_argument('customer_xid', type=str, required=True, help='Customer xid is required for wallet initialization')
 
-    wallet = api.model(
-        "wallet",
-        {
-            "amount": fields.Integer(required=True, description="The amount of money in the transaction/deposit/withdrawal"),
-            "reference_id": fields.String(description="Reference Identifier"),
-        },
-    )
+    wallet_form_parser = reqparse.RequestParser()
+    wallet_form_parser.add_argument('amount', type=int, required=True, help='Amount is required for wallet operation')
+    wallet_form_parser.add_argument('reference_id', type=str, help='Reference Identifier for the wallet operation')
 
-    disable_wallet = api.model(
-        "disable_wallet",
-        {
-            "is_disabled": fields.Boolean(required=True, description="The status of the wallet"),
-        },
-    )
+    disable_wallet_form_parser = reqparse.RequestParser()
+    disable_wallet_form_parser.add_argument('is_disabled', type=bool, required=True, help='Wallet status is required for disabling the wallet')
